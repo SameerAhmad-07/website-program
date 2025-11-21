75d45de9e0663b85b98ff50f71d622f51118f0d3
SameerAhmad-07 <sameerhallikeri4@gmail.com>
2025-11-21 12:18:31 +0530

    Update cie0002.py

--- cie0002.py
+++ cie0002.py
@@ -1,15 +1,15 @@
 import sys
 
 if len(sys.argv) == 5:
-    website  = sys.argv[1]
-    url      = sys.argv[2]
-    username = sys.argv[3]
-    password = sys.argv[4]
+    website=sys.argv[1]
+    url=sys.argv[2]
+    username=sys.argv[3]
+    password=sys.argv[4]
 else:
-    website  = "google.com"
-    url      = "https.google.com"
-    username = "Sameer Ahmad"
-    password = "sameer@07"
+    website ="google.com"
+    url="https.google.com"
+    username="Sameer Ahmad"
+    password="sameer@07"
 
 print("Login Details:")
 print(f"Website : {website}")
